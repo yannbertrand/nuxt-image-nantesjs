79e101060c723c431bc55cc329da0aa09c435c03
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-01-19 19:50:15 +0100

    :zap: Allow setting width and height to NantesJS logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,3 +1,24 @@
 <template>
-  <img class="logo" src="/nantesjs.png" alt="Logo NantesJS" />
+  <img
+    class="logo"
+    src="/nantesjs.png"
+    :width="width"
+    :height="height"
+    alt="Logo NantesJS"
+  />
 </template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      required: false,
+    },
+    height: {
+      type: Number,
+      required: false,
+    },
+  },
+}
+</script>
