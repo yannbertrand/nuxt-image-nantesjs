ce7f19d1378f267b01b0ff81f6358606be72b633
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-01-19 22:49:02 +0100

    :pencil: Add step #8 data

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
   <div style="outline: none">
     <Tooltip
-      title="Optimize external images"
-      :stepNb="7"
-      :transferedNow="244"
-      :resourcesNow="529"
-      :transferedBefore="357"
-      :resourcesBefore="640"
-      previousLink="https://step-6--nuxt-image-nantesjs.netlify.app/"
-      nextLink="https://step-8--nuxt-image-nantesjs.netlify.app/"
+      title="Add `radius` on avatars using another provider"
+      :stepNb="8"
+      :transferedNow="247"
+      :resourcesNow="531"
+      :transferedBefore="244"
+      :resourcesBefore="529"
+      previousLink="https://step-7--nuxt-image-nantesjs.netlify.app/"
+      nextLink="https://conclusion--nuxt-image-nantesjs.netlify.app/"
     />
     <section class="layout">
       <header class="header">
